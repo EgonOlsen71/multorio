4c7a5f39466f751cc8f5415c661a77fefcd5ecea
EgonOlsen <info@jpct.net>
2021-12-18 17:52:18 +0100

    Changed disk id

--- build/build.cmd
+++ build/build.cmd
@@ -1,6 +1,6 @@
 call mospeed ..\basic\multorio.bas -memhole=16192-16383 -sysbuffer=52000 -compression=true
 del multorio_AD.d64
-c1541 -format multorio,bq d64 multorio_AD.d64
+c1541 -format multorio,ml d64 multorio_AD.d64
 call ..\build\c1541 ..\build\multorio_AD.d64 -write ++multorio.prg multorio,p
 call ..\build\c1541 ..\build\multorio_AD.d64 -write universal.prg universal,p
 cd ..\build
