dc84c9af24b05821168798dfe52ee7d7803b3d71
EgonOlsen <info@jpct.net>
2021-12-19 14:47:51 +0100

    Changed image name

--- build/build-debug.cmd
+++ build/build-debug.cmd
@@ -1,6 +1,6 @@
 call mospeed ..\basic\multorio.bas -memhole=16192-16383 -generatesrc=true -sysbuffer=52000 -compression=true
-del multorio_AD.d64
-c1541 -format multorio,ml d64 multorio_AD.d64
-call ..\build\c1541 ..\build\multorio_AD.d64 -write ++multorio-c.prg multorio,p
-call ..\build\c1541 ..\build\multorio_AD.d64 -write universal.prg universal,p
+del multorio.d64
+c1541 -format multorio,ml d64 multorio.d64
+call ..\build\c1541 ..\build\multorio.d64 -write ++multorio-c.prg multorio,p
+call ..\build\c1541 ..\build\multorio.d64 -write universal.prg universal,p
 cd ..\build
--- build/build-mem.cmd
+++ build/build-mem.cmd
@@ -1,7 +1,7 @@
 call mospeed ..\basic\multorio.bas -memhole=16192-16383 -sysbuffer=52000 -compactlevel=4 -compression=true
-del multorio_AD.d64
-c1541 -format multorio,ml d64 multorio_AD.d64
-call ..\build\c1541 ..\build\multorio_AD.d64 -write ++multorio.prg multorio,p
-call ..\build\c1541 ..\build\multorio_AD.d64 -write universal.prg universal,p
+del multorio.d64
+c1541 -format multorio,ml d64 multorio.d64
+call ..\build\c1541 ..\build\multorio.d64 -write ++multorio.prg multorio,p
+call ..\build\c1541 ..\build\multorio.d64 -write universal.prg universal,p
 cd ..\build
 
