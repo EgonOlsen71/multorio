f2581ac8ef06591283c714759b9ea6659709e121
EgonOlsen <info@jpct.net>
2021-12-21 15:12:12 +0100

    Added optional title image

--- build/build.cmd
+++ build/build.cmd
@@ -1,6 +1,9 @@
 call mospeed ..\basic\multorio.bas -memhole=16192-16383 -sysbuffer=52000 -compression=true
+call mospeed ..\basic\loader.bas
+call moscrunch ++loader.prg -addfiles=..\res\title.img
 del multorio.d64
 c1541 -format multorio,ml d64 multorio.d64
+call ..\build\c1541 ..\build\multorio.d64 -write ++loader-c.prg loader,p
 call ..\build\c1541 ..\build\multorio.d64 -write ++multorio.prg multorio,p
 call ..\build\c1541 ..\build\multorio.d64 -write universal.prg universal,p
 cd ..\build
