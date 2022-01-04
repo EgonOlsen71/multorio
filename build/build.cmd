cf1b8ba50d607abc97d15dd2b2382825aead9e95
EgonOlsen <info@jpct.net>
2022-01-04 09:31:09 +0100

    Modified build script

--- build/build.cmd
+++ build/build.cmd
@@ -1,10 +1,10 @@
-call mospeed ..\basic\multorio.bas -memhole=16192-16383 -sysbuffer=52000 -compression=true
+call mospeed ..\basic\multorio.bas -memhole=16192-16383 -sysbuffer=52000
 call mospeed ..\basic\loader.bas
 call moscrunch ++loader.prg -addfiles=..\res\title.img
+call moscrunch ++multorio.prg -addfiles=..\build\universal.prg
 del multorio.d64
 c1541 -format multorio,ml d64 multorio.d64
 call ..\build\c1541 ..\build\multorio.d64 -write ++loader-c.prg loader,p
-call ..\build\c1541 ..\build\multorio.d64 -write ++multorio.prg multorio,p
-call ..\build\c1541 ..\build\multorio.d64 -write universal.prg universal,p
+call ..\build\c1541 ..\build\multorio.d64 -write ++multorio-c.prg multorio,p
 cd ..\build
 
